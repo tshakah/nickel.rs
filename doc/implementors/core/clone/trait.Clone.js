a211f82f58af61e8cc01331f0b939ccae45f8b3d
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-06-20 00:28:18 +0200

    doc(*): rebuilding docs Fr 20 Jun 2014 00:28:18 CEST

---
+++ doc/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['floor'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='floor/struct.Floor.html' title='floor::Floor'>Floor</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
